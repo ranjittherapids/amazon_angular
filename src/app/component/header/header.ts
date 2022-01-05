d059eb4c9c776c99c932b7cb06b268604a442b08
Ranjit <rapidskumar@gmail.com>
2022-01-05 09:01:46 +0530

    w

---
+++ src/app/component/header/header.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Header',
+  templateUrl: './header.html',
+  styleUrls: ['./header.css']
+})
+export class HeaderComponent  {
+
+  
+      title='hellloo man'
+  
+
+ 
+
+}
